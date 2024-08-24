3ca45b8c4d46728ffabcb4262f528ae6f1a7a8ef
Davis Vielma <davisvielma@gmail.com>
2024-08-23 22:08:48 -0400

    instalacion y prueba de paquetes

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import config
+
 def main() -> None:
-	print('hola mundo')
+	print('Mensaje con formato predeterminado')
+	print(config.RED + 'Mensaje con letra roja')
+	print(config.WHITE + 'Mensaje con letra blanca')
+	print(config.RED + 'Mensaje con letra roja antes del reseteo' + config.RESET)
+	print('Mensaje luego del reseteo')
 
 if __name__ == '__main__':
 	main()
---
+++ config.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Style
+
+RED = Fore.RED
+WHITE = Fore.WHITE
+RESET = Style.RESET_ALL
